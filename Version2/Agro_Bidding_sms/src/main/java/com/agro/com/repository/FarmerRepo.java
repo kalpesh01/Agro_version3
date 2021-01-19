69decd092849fb9652f1097a906abe1e5768eb80
kalpesh01 <42319733+kalpesh01@users.noreply.github.com>
2021-01-19 21:08:46 +0530

    version2

---
+++ Version2/Agro_Bidding_sms/src/main/java/com/agro/com/repository/FarmerRepo.java
@@ -0,0 +1,16 @@
+package com.agro.com.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.agro.com.model.Farmer;
+import com.agro.com.model.User;
+
+
+public interface FarmerRepo extends JpaRepository<Farmer, Long>{
+	
+	@Query(value="select * from farmer f where f.id=?1",nativeQuery = true)
+	public List<Farmer> getUser(long id);
+}
