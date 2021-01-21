ba7bdb62460f148bcb29ca96dbef7983a54f474e
kalpesh01 <42319733+kalpesh01@users.noreply.github.com>
2021-01-21 11:34:29 +0530

    version4

---
+++ Version4/Agro_Bidding_sms/src/main/java/com/agro/com/service/FarmerService.java
@@ -0,0 +1,51 @@
+package com.agro.com.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.agro.com.model.Farmer;
+import com.agro.com.repository.FarmerRepo;
+
+@Service
+@Transactional
+public class FarmerService {
+	@Autowired
+	private FarmerRepo frmRepo;
+	
+	
+	
+	public List<Farmer> getAllUsers()
+	{
+		return frmRepo.findAll();
+	}
+	
+	public String addFarmerProduct(Farmer frmPrdct)
+	{
+				
+		frmRepo.save(frmPrdct);
+		
+		return "success.....";
+	}
+	
+	public List<Farmer> getProductById(String usrid)
+	{
+		return frmRepo.getProducts(usrid);
+	}
+	
+	
+	public List<Farmer> getProductByPId(long pid)
+	{
+		return frmRepo.getProductByProductId(pid);
+	}
+	
+	
+	public	void updateBid(long pid,double highest_bid)
+	{
+		frmRepo.updateHighestBidPrice(pid,highest_bid);
+	}
+}
